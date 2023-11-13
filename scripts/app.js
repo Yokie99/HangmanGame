34c6567daf5fa9b3f3ae5abb27a3cc94cd4a61d4
Yokie99 <kyleym@uci.edu>
2023-11-13 11:24:21 -0800

    hangman setup

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+//Someone things of a word and we keep it secret from the other play
+// we will displau a series of underscores depending on the length of the word
+//Each turn the player will guess 1 letter from the word
+// if guess is correc we will display the letter in the blank word
+// if incorrect we draw a piece of the hangman or tell the user they have x amount of guess left.
+// add incorrect guess to a div.
+
+//start btn
+//replay btn
+// secretword
+// wrong guesses/
+// hangman
+// user guesses
+
+
+
+let startBtn = document.getElementById("startBtn");
+let restartBtn = document.getElementById("restartBtn");
+let secretWord = document.getElementById("secretWord");
+let wrongGuesses = document.getElementById("wrongGuesses");
+let hangman = document.getElementById("hangman");
+let userInput = document.getElementById("userInput");
+
+
+
+
+
+
+
+
