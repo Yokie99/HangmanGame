f4ee134394e8de582456c328e8cf940bd056469f
Yokie99 <kyleym@uci.edu>
2023-11-14 09:55:56 -0800

    game functions

--- scripts/app.js
+++ scripts/app.js
@@ -13,18 +13,52 @@
 // user guesses
 
 
-
+//ID's
 let startBtn = document.getElementById("startBtn");
 let restartBtn = document.getElementById("restartBtn");
 let secretWord = document.getElementById("secretWord");
 let wrongGuesses = document.getElementById("wrongGuesses");
 let hangman = document.getElementById("hangman");
 let userInput = document.getElementById("userInput");
+//Variables
+//random word is generated from a API
+//wrond guess will be the user;s incorrect guess
+// displaedword will be for their correct input
+let randomword = "";
+let wrongGuess = "";
+let displayedWord = []; 
 
+let guess = 0;
+let maxguess = 5;
 
+startBtn.addEventListener('click',function(){
+    APIcall();
+})
 
+function APIcall(){
+    fetch('https://random-word-api.herokuapp.com/word')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data[0]);
+            startGame(data[0])
+        })
+}
 
+function startGame(word){
+    randomword = word;
+    for(let i = 0; i < randomword.length; i++){
+        displayedWord[i] = "_";
+    }
+    updateGameState();
+}
 
+function updateGameState(){
+    secretWord.textContent = displayedWord.join(" ");
+    hangman.textContent = `Guesses Left ${guess} / ${maxguess}`
+
+}
 
 
 
